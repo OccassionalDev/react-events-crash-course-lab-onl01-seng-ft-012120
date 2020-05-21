ecc0e044f3ae319083ff857dc46146cdac0a0285
Erica Schimmel <ericaschimmel01@gmail.com>
2020-05-21 04:12:54 +0000

    Automatically backed up by Learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -28,6 +28,7 @@ export default class ChromeBoisDomain extends Component {
     return (
       <canvas 
         onMouseMove={this.handleMouseMove}
+        onClick={this.clickCycleToggle}
         width='900'
         height='600'
         tabIndex="0">
